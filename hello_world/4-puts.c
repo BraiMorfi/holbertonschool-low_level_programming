c1d6176f105b111021705e91a6160d744d6dd83c
BraiMorfi <3603@holbertonstudents.com>
2022-10-19 17:57:01 +0000

    betty documentation style fix in puts.c code

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - print "Programming is like building a multilingual puzzle". with a new line.
+ * main - print "Programming is like building a multilingual puzzle"
  * return: always 0
  */
 int main(void)
