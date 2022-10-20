46a2bbca43733f57f8e384e3df841ad35a250a55
BraiMorfi <3603@holbertonstudents.com>
2022-10-20 17:33:00 +0000

    alphabt

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*
+ * main - entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		if (low != e && low != q)
+			putchar(low);
+	}
+	printf("\n");
+	
+	return (0);
+}
