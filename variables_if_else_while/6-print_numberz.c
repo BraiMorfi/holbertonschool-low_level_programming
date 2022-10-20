8513072c479ce7439565f53e5f0c2cae56ce9eed
BraiMorfi <3603@holbertonstudents.com>
2022-10-20 18:49:43 +0000

    add worl

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ * prints all single digits number of base 10
+ * from 0
+ * Return: always 0
+ */
+int main(void)
+{
+	int num;
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	putchar('\n');
+	return (0);
+}
